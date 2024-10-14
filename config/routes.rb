8922a9d1f42aa5b267eeb0feefb5bafa6f547647
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-10-14 17:11:45 -0400

    Add Dashboard show controller, action, route

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,6 @@ Rails.application.routes.draw do
   get "login" => "sessions#new"
   post "login" => "sessions#create"
   delete "logout" => "sessions#destroy"
+
+  get "dashboard" => "dashboard#show"
 end
