e25f925b9b0c96d1109ad262a65c5c90c92cc1e6
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-10-14 17:20:57 -0400

    Add current_user, require_user methods and use in Dashboard controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  # Makes current_user method available in views. By default, all methods defined in Application Controller are available in the controllers.
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def require_user
+    redirect_to login_path unless current_user, alert: "You must be logged in to view this page"
+  end
 end
