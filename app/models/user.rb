a4c636b295f642378d177aee625fb35734a36c35
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-10-14 20:37:28 -0400

    Add email validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 end
