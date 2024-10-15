2bdbdec040ee86d4e6b7475660bef9cbe11941e9
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-10-14 21:09:27 -0400

    Add validation for password length and presence for all fields on signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   before_save :normalize_email
   
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
-
+  validates :password, presence: true, length: { minimum: 8 }, message: "must be at least 8 characters long"
+  validates :password_confirmation, presence: true, message: "must match password"
+  validates :first_name, presence: true, message: "first name is required"
+  validates :last_name, presence: true, message: "last name is required"
+  
   private
   def normalize_email
     self.email = self.email.delete(' ').downcase
