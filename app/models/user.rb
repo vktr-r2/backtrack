30ec47a4d084f123ff968e3a3d896569c0afbca7
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-10-14 20:53:57 -0400

    Add normalize_email method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
-
+  before_save :normalize_email
+  
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+
+  private
+  def normalize_email
+    self.email = self.email.delete(' ').downcase
+  end
 end
