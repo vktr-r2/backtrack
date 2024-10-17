dff34bf1471d2f109bf8b355de6005cd41918afa
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-10-16 21:50:52 -0400

    Add create and verify token methods for pw reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,24 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: :password_required?
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  # Generate a signed token with 1hr expiration time
+  def generate_password_reset_token
+    verifier.generate({user_id: id, expiring_at: 1.hours.from_now})
+  end
+
+  # Class method to verify the token and return the user
+  def self.verify_password_reset_token(token)
+    data = verifier.veried(token)
+    return if data.blank? || data[:expires_at] < Time.current
+
+    find_by(id: data[:user_id])
+  end
+
+  private
+  def self.verifiers
+    ActiveSupport::MessageVerifier.new(Rails.application.secret_key_base, digest: "SHA256")
+  end
   
   private
   def normalize_email
