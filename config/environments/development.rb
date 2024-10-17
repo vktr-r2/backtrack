76665b0e8edb8c6fba81d1f1bf01b07760ab828d
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-10-16 22:46:36 -0400

    Configure SMTP for dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,13 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "localhost",
+    port: 1025
+  }
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
